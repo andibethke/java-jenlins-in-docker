806936a79fe72b0b960628c85e51f108c4269f23
User1100 <user1100@edu.bln>
2022-07-06 10:51:00 +0200

    added Files first

---
+++ src/main/java/Start.java
@@ -0,0 +1,9 @@
+public class Start {
+    public static void main(String[] args) {
+        System.out.println(checkEven(66));
+    }
+
+    public static boolean checkEven(int no){
+        return no%2==0;
+    }
+}
---
+++ src/test/java/StartTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class StartTest {
+
+    @org.junit.jupiter.api.Test
+    void checkEven() {
+
+        assertTrue(Start.checkEven(12));
+    }
+}
